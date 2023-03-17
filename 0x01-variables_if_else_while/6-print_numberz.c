eaaa951bb8a7b29f6aeff71323bc036e190d3034
AhmedElkhodary <ahmed.alkhodary99@gmail.com>
2023-03-17 13:14:30 -0700

    print digits 0..9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main -  prints numbers from 0 to 9
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(48 - i);
+	}
+	putchar ('\n');
+	return (0);
+}
+
