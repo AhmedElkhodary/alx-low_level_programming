a1de181efe2d41fd220a125e16c147a2132bdb1c
AhmedElkhodary <ahmed.alkhodary99@gmail.com>
2023-03-17 13:07:10 -0700

    print digits from 1 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -  prints numbers from 0 to 9
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	for (int i = 0; i <= 9; i++)
+	{
+		printf("%d", i);
+	}
+	putchar ('\n');
+	return (0);
+}
+
