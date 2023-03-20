57a7cf363abf33ea9afd3452ede3e5019f0a1cf3
AhmedElkhodary <ahmed.alkhodary99@gmail.com>
2023-03-20 05:12:49 -0700

    prit_comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+/**
+ * main - pritn digits
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j, k, m;
+
+	i = 0;
+	while (i <= 9)
+	{
+		j = 0;
+		while (j <= 9)
+		{
+			k = 0;
+			while (k <= 9)
+			{
+				m = 0;
+				while (m <= 9)
+				{
+					if ((i * 10) + j < (k * 10) + m)
+					{
+						putchar(i + 48);
+						putchar(j + 48);
+						putchar(' ');
+						putchar(k + 48);
+						putchar(m + 48);
+						if (i == 9 && j == 8 && k == 9 && m == 9)
+						{
+							break;
+						}
+						putchar(',');
+						putchar(' ');
+					}
+					m++;
+				}
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
