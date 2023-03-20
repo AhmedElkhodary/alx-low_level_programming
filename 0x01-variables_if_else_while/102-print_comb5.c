513f4d0c54d233e5731d1cd5511cd03aea257427
AhmedElkhodary <ahmed.alkhodary99@gmail.com>
2023-03-20 05:29:24 -0700

    prit_comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,42 +6,40 @@
  */
 int main(void)
 {
-	int i, j, k, m;
+int i, j, k, m;
 
-	i = 0;
-	while (i <= 9)
+i = 0;
+while (i <= 9)
+{
+	j = 0;
+	while (j <= 9)
 	{
-		j = 0;
-		while (j <= 9)
+		k = 0;
+		while (k <= 9)
 		{
-			k = 0;
-			while (k <= 9)
+			m = 0;
+			while (m <= 9)
 			{
-				m = 0;
-				while (m <= 9)
+				if ((i * 10) + j < (k * 10) + m)
 				{
-					if ((i * 10) + j < (k * 10) + m)
-					{
-						putchar(i + 48);
-						putchar(j + 48);
-						putchar(' ');
-						putchar(k + 48);
-						putchar(m + 48);
-						if (i == 9 && j == 8 && k == 9 && m == 9)
-						{
-							break;
-						}
-						putchar(',');
-						putchar(' ');
-					}
-					m++;
+					putchar(i + 48);
+					putchar(j + 48);
+					putchar(' ');
+					putchar(k + 48);
+					putchar(m + 48);
+					if (i == 9 && j == 8 && k == 9 && m == 9)
+						break;
+					putchar(',');
+					putchar(' ');
 				}
-				k++;
+				m++;
 			}
-			j++;
+			k++;
 		}
-		i++;
+		j++;
 	}
-	putchar('\n');
-	return (0);
+	i++;
+}
+putchar('\n');
+return (0);
 }
