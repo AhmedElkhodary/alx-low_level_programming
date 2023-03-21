060dafe7c806217e0ffbed6b89ca585cc20fd41a
AhmedElkhodary <ahmed.alkhodary99@gmail.com>
2023-03-21 16:41:12 -0700

    Adding: isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isalpha - checks for lowercase character
+ *
+ * @c: Nubmer Ascii code for some character
+ *
+ * Return: 1 (is alphbet)   0 (is not alphabet)
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+		return (1);
+	return (0);
+}
