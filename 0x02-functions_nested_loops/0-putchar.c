db3eaa9df8deeee4607dd9419be470e2da3ecceb
AhmedElkhodary <ahmed.alkhodary99@gmail.com>
2023-03-21 04:02:23 -0700

    Adding: 0-putchar.c first commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.c"
+/**
+ * main - print(_putchar) followed by a new line
+ *
+ * Return: on success 1
+ */
+int main(void)
+{
+	int i;
+
+	char[] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(char[i]);
+	}
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,5 @@
+#ifndef _main_h
+#define _main_h
+void _putchar(char);
+
+#endif
