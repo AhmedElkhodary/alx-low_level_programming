9c04069b04fc6800168546717c61bae1a525aa85
AhmedElkhodary <ahmed.alkhodary99@gmail.com>
2023-03-21 17:01:28 -0700

    Adding: 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_sign - checks weather number is (negative,zero or positive)
+ *
+ * @n: number that input to calculate
+ *
+ * Return: 0 (zero)  1(positive) -1(negative)
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return ('-1');
+	}
+	else
+	{
+		_putchar('0');
+		return ('0');
+	}
+}
