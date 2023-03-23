c172c140dc96ef422e1749ee085c3cf98156394c
AhmedElkhodary <ahmed.alkhodary99@gmail.com>
2023-03-22 19:17:41 -0700

    Adding: 9-times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,13 +10,21 @@ void times_table(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		_putchar('48');
+		for (j = 1; j <= 9; j++)
 		{
-			_putchar('0' + i * j);
-			if (j == 9)
-				continue;
 			_putchar(',');
 			_putchar(' ');
+			if (i * j < 10)
+			{
+				_putchar(' ');
+				_putchar('0' + i * j);
+			}
+			else
+			{
+				_putchar('0' + (i * j) / 10);
+				_putchar('0' + (i * j) % 10);
+			}
 		}
 		_putchar('\n');
 	}
