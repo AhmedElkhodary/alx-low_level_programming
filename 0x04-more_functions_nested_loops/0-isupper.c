f1e5b85805c23bce7af021ccbb89796c18c6bbe6
AhmedElkhodary <ahmed.alkhodary99@gmail.com>
2023-03-23 19:39:17 -0700

    Adding: isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - check character is uppercase or not
+ *
+ * @c: character
+ *
+ * Return: 1(upercase)  0(otherwise)
+ */
+
+int main(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+
+	return (0);
+}
