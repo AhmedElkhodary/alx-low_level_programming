3c93668d9fdc73882f8548ac7e52cfd36156bd7d
AhmedElkhodary <ahmed.alkhodary99@gmail.com>
2023-03-24 19:34:34 -0700

    Adding: 9-fizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,23 +8,23 @@
  * Return: 0 (success)
  */
 
-void main(void)
+int main(void)
 {
 	int i;
 
 	for (i = 1; i <= 100; i++)
 	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+			printf(" FizzBuzz ");
 		if ((i % 3) == 0)
-			printf("Fizz");
+			printf(" Fizz ");
 		else if ((i % 5) == 0)
-			printf("Buzz");
-		else if (((i % 3) == 0) && ((i % 5) == 0))
-			printf("FizzBuzz");
+			printf(" Buzz ");
 		else
 		{
-			printf("%d", i);
+			printf("%d ", i);
 		}
 	}
-	_putchar('\n');
+	printf("\n");
 	return (0);
 }
